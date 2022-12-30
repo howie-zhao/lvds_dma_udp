52eae6e3b277d056737f256cc72f24c33b3fcc1a
howie-zhao <1531549264@qq.com>
2022-12-30 17:58:50 +0800

    add axi stream interface

--- lvds_recv.v
+++ lvds_recv.v
@@ -2,7 +2,9 @@
 
 /**************************************************************************************/
 module lvds_recv #(
-    parameter DW = 15
+    parameter DW = 15,
+    parameter FRAME_NUM = 1024,
+    parameter AXI_DW = 32
 )(
     input wire [DW-1:0] DATA_P, //输入15位数据信号的p端
     input wire [DW-1:0] DATA_N, //输入15位数据信号的n端
@@ -13,12 +15,21 @@ module lvds_recv #(
     inout wire SDA,
 `endif
     // Clock and reset
-    input wire clk, //模块时钟
+    input wire clk, //模块时钟 100M
     input wire rst_n, //模块复位信号
-    input wire CLK_P, //来自A1100的输入时钟，相对于pdata有一定的滞后
+    input wire CLK_P, //来自A1100的输入时钟，相对于pdata有一定的滞后 频率：250M 125M 62.5M
     input wire CLK_N, 
     output wire RST_D, //给A1100数字部分的复位信号 RSTN
-    output wire RST_A  //给A1100模拟部分的复位信号 XSHUTDOWN
+    output wire RST_A,  //给A1100模拟部分的复位信号 XSHUTDOWN
+    // AXI Stream Interface
+    input wire m_aclk,
+    input wire m_aresetn,
+    input wire m_axis_tready,
+    output wire m_axis_tvalid,
+    output wire m_axis_tlast,
+    //output wire m_axis_tuser,
+    //output wire m_axis_tkeep,
+    output wire [AXI_DW-1:0] m_axis_tdata
 );
 /**************************************************************************************/
 `ifndef ZYNQ_PS_EMIO_IIC
@@ -35,6 +46,8 @@ module lvds_recv #(
     wire CLK_IN; //单端时钟信号
     reg [DW-1:0] data_r;
     reg group_r;
+    wire [DW:0] fifo_wr_data;
+    reg fifo_wr_en;
 /**************************************************************************************/
 //差分输入转单端
     genvar i;
@@ -55,7 +68,7 @@ module lvds_recv #(
             .DIFF_TERM("TRUE"),
             .IOSTANDARD("LVDS_25")
         ) u_cbufds (
-            .O(CLK_IN),
+            .O(CLK_IN), // 250M max
             .I(CLK_P),
             .IB(CLK_N)
         );
@@ -71,13 +84,87 @@ module lvds_recv #(
             group_r <= D_GROUP;     
         end
     end
+    assign fifo_wr_data = {group_r, data_r};
+    always @(posedge CLK_IN or negedge rst_n) begin
+        if(!rst_n) begin
+            fifo_wr_en <= 1'b0;
+        end
+        else if (fifo_wr_cnt != 0) begin
+            fifo_wr_en <= 1'b1;
+        end
+        else begin
+            fifo_wr_en <= 1'b0;
+        end
+    end
+
+    always @(posedge CLK_IN or negedge rst_n) begin
+        if(!rst_n) begin
+            fifo_wr_cnt <= 10'b0;
+        end
+        else if (fifo_wr_cnt == 512) begin //一次传输有512+1个时钟周期，第一个周期无效
+            fifo_wr_cnt <= 10'b0;
+        end
+        else begin
+            fifo_wr_cnt <= fifo_wr_cnt + 1;
+        end
+    end
 /**************************************************************************************/
 //异步FIFO，packet mode
-
+    fifo u_fifo_recv(
+        .wr_clk       (CLK_IN),
+        .rd_clk       (m_aclk),
+        .srst         (~rst_n),
+        .wr_en        (fifo_wr_en),
+        .rd_en        (fifo_rd_en),
+        .din          (fifo_wr_data),
+        .dout         (fifo_rd_data),
+        .empty        (empty),
+        .full         (full),
+        .overflow     (overflow),
+        .underflow    (underflow),
+        .rd_data_count(rd_data_cnt)
+    );
 /**************************************************************************************/
 //FIFO的 AXI stream 接口设计
 //1帧数据写入后，拉高输出valid，等待后续DMA的ready（read enable）信号
 //需要在一帧数据结束时拉高tlast信号
 
+    assign m_axis_tvalid = fifo_rd_en_d[1]; //读FIFO有1个cycle的latency
+    assign m_axis_tlast = tx_done;
+    assign m_axis_tdata = tx_data;
 
+    always @(posedge m_aclk or negedge m_aresetn) begin
+        if(!m_aresetn) begin
+            tx_valid <= 0;
+        end
+        else if(rd_data_cnt >= FRAME_NUM) begin
+            tx_valid <= 1;
+        end
+        else if(tx_done) begin
+            tx_valid <= 0;
+        end
+    end
+    assign fifo_rd_en = tx_valid & m_axis_tready;
+    always @(posedge m_aclk or negedge m_aresetn) begin
+        if(!m_aresetn) begin
+            fifo_rd_en_d[1:0] <= 0;
+        end
+        else begin
+            fifo_rd_en_d[1:0] <= {fifo_rd_en_d[0], fifo_rd_en};
+        end
+    end
+    always @(posedge m_aclk or negedge m_aresetn) begin
+        if(!m_aresetn) begin
+            tx_data <= 0;
+            tx_cnt <= 0;
+        end
+        else if(fifo_rd_en_d[0]) begin
+            tx_data <= fifo_rd_data;
+            tx_cnt <= tx_cnt + 1;
+        end
+        else if (tx_done) begin
+            tx_cnt <= 0;
+        end
+    end
+    assign tx_done = (tx_cnt == FRAME_NUM);
 endmodule //lvds_recv
